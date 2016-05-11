055232d73d42d24548de66a846e4191adcc6e13c
Eric Heydrick <eheydrick@gmail.com>
2016-05-11 12:04:07 -0700

    fix rubocop issue

--- lib/sensu-plugins-irc/version.rb
+++ lib/sensu-plugins-irc/version.rb
@@ -4,6 +4,6 @@ module SensuPluginsIrc
     MINOR = 0
     PATCH = 1
 
-      VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
 end
